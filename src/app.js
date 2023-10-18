c6c98b6f5240fd8be966aea36dfaab7a721749e0
juliangoaguirre <143230248+juliangoaguirre@users.noreply.github.com>
2023-10-18 17:46:39 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let element = document.querySelector("#mydiv");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domainNames = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log("first for loop");
+    console.log("this is i: " + i);
+    for (let j = 0; j < adj.length; j++) {
+      console.log("second for loop");
+      console.log("this is i: " + i + " this is j: " + j);
+      for (let k = 0; k < adj.length; k++) {
+        console.log("third for loop");
+        console.log(
+          "this is i: " + i + " this is j: " + j + " this is k: " + k
+        );
+        domainNames.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  for (let i = 0; i < domainNames.length; i++) {
+    element.innerHTML += "<div>" + domainNames[i] + "</div>";
+  }
 };
